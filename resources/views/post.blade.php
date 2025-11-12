2501a0fa09ff9aa314f0d6e5e7168635c71002c0
LukasKrisna <krisnaprasriyan@gmail.com>
2025-11-12 12:07:51 +0700

    single blog post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,12 @@
 <x-layout :title="$title">
     <div class="space-y-6">
-        @foreach ($posts as $post)
         <article class="bg-gray-800 rounded-lg p-6 shadow-lg">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
             <h2 class="text-2xl font-bold text-white mb-2">{{ $post['title'] }}</h2>
-            </a>
             <div class="text-gray-400 text-sm mb-4">
                 <a href="#" class="hover:text-white">{{ $post['author'] }}</a> | {{ $post['date'] }}
             </div>
-            <p class="text-gray-300 mb-4 leading-relaxed">{{ Str::limit($post['body'], 100) }}</p>
-            <a href="/posts/{{ $post['slug'] }}" class="text-indigo-400 hover:text-indigo-300 font-medium">Read more &raquo;</a>
+            <p class="text-gray-300 mb-4 leading-relaxed">{{ $post['body'] }}</p>
+            <a href="/posts" class="text-indigo-400 hover:text-indigo-300 font-medium">&laquo; Back to all posts.</a>
         </article>
-        @endforeach
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
@@ -9,18 +10,24 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'first-post',
             'title' => 'First Post', 
             'author' => 'Alice',
             'date' => '1 day ago',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tempora fugit rem eos illo quo sit culpa, laborum corporis earum rerum, sint quia, delectus accusamus magnam est? Soluta, odio placeat?'
         ],
         [
+            'id' => 2,
+            'slug' => 'second-post',
             'title' => 'Second Post', 
             'author' => 'Bob',
             'date' => '3 days ago',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eius necessitatibus culpa a asperiores nostrum aperiam. Debitis consectetur a magni veniam vel accusamus culpa sed, similique officia unde non aut.'
         ],
         [
+            'id' => 3,
+            'slug' => 'third-post',
             'title' => 'Third Post', 
             'author' => 'Charlie',
             'date' => '1 week ago',
@@ -29,6 +36,45 @@ Route::get('/posts', function () {
     ];
 
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'first-post',
+            'title' => 'First Post', 
+            'author' => 'Alice',
+            'date' => '1 day ago',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tempora fugit rem eos illo quo sit culpa, laborum corporis earum rerum, sint quia, delectus accusamus magnam est? Soluta, odio placeat?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'second-post',
+            'title' => 'Second Post', 
+            'author' => 'Bob',
+            'date' => '3 days ago',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eius necessitatibus culpa a asperiores nostrum aperiam. Debitis consectetur a magni veniam vel accusamus culpa sed, similique officia unde non aut.'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'third-post',
+            'title' => 'Third Post', 
+            'author' => 'Charlie',
+            'date' => '1 week ago',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sit facilis nam et adipisci eligendi officia perferendis, quia est consequatur dolore dolores eos, ut numquam explicabo praesentium sunt repellendus quas!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if (! $post) {
+        abort(404);
+    }
+    
+    return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
 Route::get('/about', function () {

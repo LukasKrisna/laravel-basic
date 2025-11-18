21d2cf027bcee225445badc5102e7cef1dda5e25
LukasKrisna <krisnaprasriyan@gmail.com>
2025-11-18 21:57:46 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,40 +1,8 @@
 <?php
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'first-post',
-                'title' => 'First Post', 
-                'author' => 'Alice',
-                'date' => '1 day ago',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tempora fugit rem eos illo quo sit culpa, laborum corporis earum rerum, sint quia, delectus accusamus magnam est? Soluta, odio placeat?'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'second-post',
-                'title' => 'Second Post', 
-                'author' => 'Bob',
-                'date' => '3 days ago',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eius necessitatibus culpa a asperiores nostrum aperiam. Debitis consectetur a magni veniam vel accusamus culpa sed, similique officia unde non aut.'
-            ],
-            [
-                'id' => 3,
-                'slug' => 'third-post',
-                'title' => 'Third Post', 
-                'author' => 'Charlie',
-                'date' => '1 week ago',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sit facilis nam et adipisci eligendi officia perferendis, quia est consequatur dolore dolores eos, ut numquam explicabo praesentium sunt repellendus quas!'
-            ],
-        ];
-    }
-
-    public static function find($slug){
-        return Arr::first(static::all(), fn($post) => $post['slug'] === $slug) ?? abort(404);
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'first-post',
-            'title' => 'First Post', 
-            'author' => 'Alice',
-            'date' => '1 day ago',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tempora fugit rem eos illo quo sit culpa, laborum corporis earum rerum, sint quia, delectus accusamus magnam est? Soluta, odio placeat?'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'second-post',
-            'title' => 'Second Post', 
-            'author' => 'Bob',
-            'date' => '3 days ago',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eius necessitatibus culpa a asperiores nostrum aperiam. Debitis consectetur a magni veniam vel accusamus culpa sed, similique officia unde non aut.'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'third-post',
-            'title' => 'Third Post', 
-            'author' => 'Charlie',
-            'date' => '1 week ago',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sit facilis nam et adipisci eligendi officia perferendis, quia est consequatur dolore dolores eos, ut numquam explicabo praesentium sunt repellendus quas!'
-        ],
-    ];
+    $posts = Post::all();
 
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });

05b41f0d193e8c7690e45f68716fc1c179ae1933
LukasKrisna <krisnaprasriyan@gmail.com>
2025-11-27 00:01:31 +0700

    Tinker & Model Factories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
         <article class="bg-gray-800 rounded-lg p-6 shadow-lg">
             <h2 class="text-2xl font-bold text-white mb-2">{{ $post['title'] }}</h2>
             <div class="text-gray-400 text-sm mb-4">
-                <a href="#" class="hover:text-white">{{ $post['author'] }}</a> | {{ $post['date'] }}
+                <a href="#" class="hover:text-white">{{ $post['author'] }}</a> | 1 January 2025
             </div>
             <p class="text-gray-300 mb-4 leading-relaxed">{{ $post['body'] }}</p>
             <a href="/posts" class="text-indigo-400 hover:text-indigo-300 font-medium">&laquo; Back to all posts.</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
             <h2 class="text-2xl font-bold text-white mb-2">{{ $post['title'] }}</h2>
             </a>
             <div class="text-gray-400 text-sm mb-4">
-                <a href="#" class="hover:text-white">{{ $post['author'] }}</a> | {{ $post['date'] }}
+                <a href="#" class="hover:text-white">{{ $post['author'] }}</a> | 1 January 2025
             </div>
             <p class="text-gray-300 mb-4 leading-relaxed">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="text-indigo-400 hover:text-indigo-300 font-medium">Read more &raquo;</a>

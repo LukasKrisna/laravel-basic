0f05896826e85a9f3699c6ffd2a335f141eb1b94
LukasKrisna <krisnaprasriyan@gmail.com>
2025-12-02 06:50:11 +0700

    simple post search

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,7 +13,21 @@
         </article>
         @endforeach --}}
 
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+  <div class="py-4 px-4 mx-auto max-w-screen-xl lg:px-6">
+
+
+    <form class="mb-8 max-w-md mx-auto">   
+        <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
+            </div>
+            <input type="search" id="search" class="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder="Search post title..." required autofocus/>
+            <button type="button" class="absolute end-1.5 bottom-1.5 text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs px-3 py-1.5 focus:outline-none">Search</button>
+        </div>
+    </form>
+
+
       <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
         @foreach ($posts as $post)
           <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">

7f6bc610b074e1b310a943338a572254850df912
LukasKrisna <krisnaprasriyan@gmail.com>
2025-12-03 02:32:00 +0700

    unused routes

--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
-Route::get('/authors/{user:username}', function (User $user) {
-    // $posts = $user->posts->load('category', 'author'); // Lazy Eager Loading
+// Route::get('/authors/{user:username}', function (User $user) {
+//     // $posts = $user->posts->load('category', 'author'); // Lazy Eager Loading
 
-    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
-});
+//     return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
+// });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load('category', 'author'); // Lazy Eager Loading
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     // $posts = $category->posts->load('category', 'author'); // Lazy Eager Loading
 
-    return view('posts', ['title' => 'Category: ' . $category->name, 'posts' => $category->posts]);
-});
+//     return view('posts', ['title' => 'Category: ' . $category->name, 'posts' => $category->posts]);
+// });
 
 Route::get('/about', function () {
     return view('about', ['title' => 'About Us']);

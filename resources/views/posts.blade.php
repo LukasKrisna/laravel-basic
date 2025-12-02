77af00c08d7ee909334e6dc9b387556a4fed770a
LukasKrisna <krisnaprasriyan@gmail.com>
2025-12-02 07:02:58 +0700

    fix image and author name design

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -42,13 +42,11 @@
               <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
               <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post['body'], 100) }}</p>
               <div class="flex justify-between items-center">
-                  <div class="flex items-center space-x-4">
-                      <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt={{ $post->author->name }} />
-                      <a href="/authors/{{ $post->author->username }}" class="font-medium text-xs dark:text-white">
-                          {{ $post->author->name }}
-                      </a>
-                  </div>
-                  <a href="/posts/{{ $post['slug'] }}" class="inline-flex items-center font-medium text-xs text-primary-600 dark:text-primary-500 hover:underline">
+                <a href="/authors/{{ $post->author->username }}" class="flex items-center space-x-2 hover:underline" rel="author">
+                    <img class="w-7 h-7 rounded-full object-cover flex-shrink-0" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="{{ $post->author->name }}" />
+                    <span class="font-medium text-xs dark:text-white truncate">{{ $post->author->name }}</span>
+                </a>
+                  <a href="/posts/{{ $post['slug'] }}" class="inline-flex items-center font-medium text-xs text-primary-600 dark:text-primary-500 hover:underline flex-shrink-0">
                       Read more
                       <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
